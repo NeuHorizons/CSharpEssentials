88ce71066b0cca8d717e745f7fbf7e796d95cdd3
NeuHorizons <camhengland@gmail.com>
2024-05-14 23:03:54 -0600

    Working on scene

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -7,3 +7,32 @@
 //Add another class with any name, add variables 
 //Using the new keyword create several objects
 //Console writeline the Objects variables
+using System;
+					
+public class Program
+{
+ public Health PresidentsHealth;
+ public Damage Bullet;
+	
+	public void Main()
+	{
+        PresidentsHealth = new Health();
+		Bullet = new Damage();
+
+        
+		Health.PresidentsHealth -= Damage.Bullet;
+		Console.WriteLine("Hello Wor... IM ALIVE");
+        Console.WriteLine("GET DOWN MR PRESIDENT!!!");
+        Console.WriteLine("BANG!!!");
+        Console.WriteLine("Health.Name + " WARNING HEALTH HALF WAY!!! " + Health.PresidentsHealth + ".");
+
+	}
+public class Health { 
+	public int PresidentsHealth = 50;
+    public string Name = "President";
+}
+public class Damage { 
+	public int Bullet = 25;
+}
+
+}
